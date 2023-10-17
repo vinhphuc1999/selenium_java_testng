6027369c44d2ec727bf04975750c02d0124017cb
Vĩnh Phúc <pvphuc999@gmail.com>
2023-10-17 22:19:22 +0700

    Done_Topic 3_Part 3

---
+++ src/testng/Topic_01_Assert.java
@@ -0,0 +1,19 @@
+package testng;
+
+import org.testng.Assert;
+
+public class Topic_01_Assert {
+
+	public static void main(String[] args) {
+		String fullname = "Automation Testing";
+		// TODO Auto-generated method stub
+		// Assert
+		// Mong đợi 1 điều kiện trả về ĐÚNG
+		Assert.assertTrue(fullname.contains("Automation"));
+		// Mong đợi 1 điều kiện trả về SAI
+		Assert.assertFalse(fullname.contains("Manual"));
+		// Mong đợi bằng nhau
+		Assert.assertEquals(fullname, "Automation Testing");
+	}
+
+}
